91ea71b10ad299854303a5d40682612ef9e61871
ragamuffin <raga@raspi.(none)>
2013-12-09 22:52:09 +0100

    001-display

--- i2c/lcd.py
+++ i2c/lcd.py
@@ -8,10 +8,14 @@
 
 # I2C address of Display HD44780 is: 0x27
 
-import lcddriver
+import lcddriver1
 from time import *
 
-lcd = lcddriver.lcd()
+lcd = lcddriver1.lcd()
 
-lcd.lcd_display_string("AAAAAAAAAAAAAAAAAAAACCCCCCCCCCCCCCCCCCCCBBBBBBBBBBBBBBBBBBBBDDDDDDDDDDDDDDDDDDDD", 1)
-
+#lcd.lcd_display_string("AAAAAAAAAAAAAAAAAAAACCCCCCCCCCCCCCCCCCCCBBBBBBBBBBBBBBBBBBBBDDDDDDDDDDDDDDDDDDDD", 1)
+ 
+lcd.lcd_display_string("*** Hello world ***", 1)
+lcd.lcd_display_string("My name is Raspi :-)", 2)
+lcd.lcd_display_string("*******************", 3)
+lcd.lcd_display_string("***** The Best *****", 4)
